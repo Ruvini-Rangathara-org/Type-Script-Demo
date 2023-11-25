8d6c62bdd1fdf2732a1ba27233c13f754b72624f
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-11-25 16:00:59 +0530

    case 10

---
+++ 10_tuples.ts
@@ -0,0 +1,9 @@
+//Tuple - A tuple is a collection which is ordered and unchangeable.
+//allows duplicate members
+//can have different data types
+
+let arr: [number, string, boolean] = [1, "hello", true];
+console.log(arr);
+
+let arr1: [number, string, number,boolean] = [1, "hello",5, true];
+console.log(arr1);
