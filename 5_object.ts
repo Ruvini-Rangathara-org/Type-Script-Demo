db10a4f51616aa5e823e7d36886cf3a8e67cc049
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-11-25 12:59:05 +0530

    case 5

---
+++ 5_object.ts
@@ -0,0 +1,21 @@
+//objects
+
+
+let student:{
+    id:number,
+    name:string,
+    age:number,
+    college:string
+};
+
+student= {
+    id:1, name:'Ruvini', age:23, college:'IJSE'
+};
+
+//compile error
+//student = 10;
+// student= {id:1, name:'Ruvini', age:23, college:'IJSE',subject:'SE'};
+
+
+console.log(student);
+
--- app.js
+++ app.js
@@ -1,34 +1,31 @@
-//functions
-//return type is number
-function findSum(num1, num2) {
-    return num1 + num2;
-}
-var result = findSum(10, 30);
-console.log("Result : " + result);
-////////////////////////////////////////////////////////////////////
-function isMax(num2) {
-    var num1 = 20;
-    return num1 < num2;
-}
-var num2 = 10;
-var result2 = isMax(num2);
-console.log(result2 ? "".concat(num2, " is Max") : "".concat(num2, " is Min"));
-////////////////////////////////////////////////////////////////////
-//arrow function
-var findSumInString = function (num1, num2) {
-    return "Sum is ".concat(num1 + num2);
+//objects
+var student;
+student = {
+    id: 1, name: 'Ruvini', age: 23, college: 'IJSE'
 };
-console.log(findSumInString(10, 89));
-///////////////////////////////////////////////////////////////////
-function find(num1, num2) {
-    if (typeof num1 === 'string' || typeof num2 === 'string') { // type guard
-        return +num1 + +num2; // convert to numbers
-    }
-    else {
-        num1 + num2;
-    }
+//compile error
+//student = 10;
+// student= {id:1, name:'Ruvini', age:23, college:'IJSE',subject:'SE'};
+// console.log(student);
+//------------------------------------- distance calculator -------------------------------
+// method for calculate distance between two locations
+function calcDistance(location1, location2) {
+    //calculate distance
+    var R = 6371e3; // metres
+    var φ1 = location1.lat * Math.PI / 180; // φ, λ in radians
+    var φ2 = location2.lat * Math.PI / 180;
+    var Δφ = (location2.lat - location1.lat) * Math.PI / 180;
+    var Δλ = (location2.lon - location1.lon) * Math.PI / 180;
+    var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c; // in metres
+    return d;
 }
-var number1 = find(30, 90);
-console.log('find : ' + number1);
-var number2 = find('45', 90);
-console.log('find : ' + number2);
+var location1 = { lon: 12.9716, lat: 77.5946 }; //bangalore
+var location2 = { lon: 13.0827, lat: 80.2707 }; //chennai
+var location3 = { lon: 6.9271, lat: 79.8612 }; //colombo
+var location4 = { lon: 51.5074, lat: 0.1278 }; //london
+console.log('Chennai to Bangalore distance: ' + calcDistance(location1, location2).toFixed(2));
+console.log('Colombo to London distance: ' + calcDistance(location3, location4).toFixed(2));
